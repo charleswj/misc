4c6e11effb084c8d3a9a302b533ecfd6c01a3f64
Charles Jones <32230880+charleswj@users.noreply.github.com>
2025-07-25 18:02:33 -0500

    Add files via upload

---
+++ GetNewExoMailboxAuditValues.ps1
@@ -0,0 +1,44 @@
+# "SearchQueryInitiated" removed for illustration as a "new" value
+$KnownValues = @(
+               'AddFolderPermissions'
+               'ApplyPriorityCleanup'
+               'ApplyRecord'
+               'AttachmentAccess'
+               'Copy'
+               'Create'
+               'Default'
+               'FolderBind'
+               'HardDelete'
+               'MailboxLogin'
+               'MailItemsAccessed'
+               'MessageBind'
+               'ModifyFolderPermissions'
+               'Move'
+               'MoveToDeletedItems'
+               'None'
+               'PreservedMailItemProactively'
+               'PriorityCleanupDelete'
+               'RecordDelete'
+               'RemoveFolderPermissions'
+               'Send'
+               'SendAs'
+               'SendOnBehalf'
+               'SoftDelete'
+               'Update'
+               'UpdateCalendarDelegation'
+               'UpdateComplianceTag'
+               'UpdateFolderPermissions'
+               'UpdateInboxRules'
+)
+
+try
+{
+    # any mailbox name and audit value can be used, the following works as is
+    Set-Mailbox BABABOOEY -AuditOwner @{Add='BABABOOEY'} -ErrorAction Stop
+}
+catch
+{
+    [void]($_.ToString() -match 'The possible enumeration values are ''(?<values>[^'']*)''')
+
+    (Compare-Object ($Matches.Item('values') -split ', ') $KnownValues).InputObject | sort
+}
